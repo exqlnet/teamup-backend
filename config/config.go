acc9c1648164a3ad275a10577aa32f876c6da75d
Exqlnet <exqlnet@gmail.com>
2020-04-26 17:30:22 +0800

    :wrench: Distinguish dev and prod

--- config/config.go
+++ config/config.go
@@ -2,11 +2,20 @@ package config
 
 import (
 	"github.com/micro/go-micro/v2/config"
+	"log"
+	"os"
 )
 
 var Cfg config.Config
 
 func init() {
 	Cfg, _ = config.NewConfig()
-	config.LoadFile("config/config.json")
+	if os.Getenv("GO_RUN_MODE") == "product" {
+		config.LoadFile("config/config.json")
+		log.Println("Running at product mode")
+	} else {
+		config.LoadFile("config/config-dev.json")
+		log.Println("Running at dev mode")
+	}
+
 }
