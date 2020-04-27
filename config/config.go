84415455cc693bad3260a38b97b8317146818766
Exqlnet <exqlnet@gmail.com>
2020-04-27 21:17:54 +0800

    :bug: fucking go config

--- config/config.go
+++ config/config.go
@@ -7,15 +7,31 @@ import (
 )
 
 var Cfg config.Config
+var CfgMap map[string]interface{}
 
 func init() {
-	Cfg, _ = config.NewConfig()
-	if os.Getenv("GO_RUN_MODE") == "product" {
-		config.LoadFile("config/config.json")
+	Cfg  = config.DefaultConfig
+	cwd, _ := os.Getwd()
+	log.Println("Current dir is", cwd)
+	cfgDir := cwd + string(os.PathSeparator) + "config" + string(os.PathSeparator)
+	var err error
+	switch os.Getenv("GO_RUN_MODE") {
+	case "product":
+		err = config.LoadFile(cfgDir + "config.json")
 		log.Println("Running at product mode")
-	} else {
-		config.LoadFile("config/config-dev.json")
+	case "dev":
+		err = config.LoadFile(cfgDir + "config-dev.json")
+		log.Println("Running at dev mode")
+	case "test":
+		err = config.LoadFile(cfgDir + "config-test.json")
+		log.Println("Running at test mode")
+	default:
+		err = config.LoadFile(cfgDir + "config-dev.json")
 		log.Println("Running at dev mode")
 	}
 
+	if err != nil {
+		log.Fatal(err)
+	}
+	CfgMap = Cfg.Map()
 }
