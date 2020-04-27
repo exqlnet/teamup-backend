48c1d019cbfeb45fa476cf9d619b2b06bf7e0666
Exqlnet <exqlnet@gmail.com>
2020-04-27 22:39:23 +0800

    :bug: fucking go config

--- config/config.go
+++ config/config.go
@@ -2,36 +2,40 @@ package config
 
 import (
 	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-micro/v2/config/source"
+	"github.com/micro/go-micro/v2/config/source/memory"
 	"log"
 	"os"
 )
 
 var Cfg config.Config
-var CfgMap map[string]interface{}
 
 func init() {
 	Cfg  = config.DefaultConfig
-	cwd, _ := os.Getwd()
-	log.Println("Current dir is", cwd)
-	cfgDir := cwd + string(os.PathSeparator) + "config" + string(os.PathSeparator)
 	var err error
+
+	var sc source.Source
+	var cfgData []byte
+
 	switch os.Getenv("GO_RUN_MODE") {
 	case "product":
-		err = config.LoadFile(cfgDir + "config.json")
+		cfgData, err = Asset("config.json")
 		log.Println("Running at product mode")
 	case "dev":
-		err = config.LoadFile(cfgDir + "config-dev.json")
+		cfgData, err = Asset("config-dev.json")
 		log.Println("Running at dev mode")
 	case "test":
-		err = config.LoadFile(cfgDir + "config-test.json")
+		cfgData, err = Asset("config-test.json")
 		log.Println("Running at test mode")
 	default:
-		err = config.LoadFile(cfgDir + "config-dev.json")
+		cfgData, err = Asset("config-dev.json")
 		log.Println("Running at dev mode")
 	}
+
+	sc = memory.NewSource(memory.WithJSON(cfgData))
+	err = config.Load(sc)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	CfgMap = Cfg.Map()
 }
